bb6aa0738e5ff7921fd902bf7773c0ea7692b1b8
jamesnampy <jamesnampalam@gmail.com>
2026-01-03 22:19:49 -0800

    Update App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 import { ProtectedRoute } from './components/common/ProtectedRoute';
